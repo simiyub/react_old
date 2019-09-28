c3cd4561c86da756e00821da82b7d6466df0e57d
bramwell simiyu <simiyub.engineer@gmail.com>
2019-09-28 07:35:38 +0100

    Updates

---
+++ precache-manifest.302ed6e4ef2c22dbfdf0b5beaaa3b2e6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4d2f67047a66dfe43d4ed7a1fcc4d4c",
+    "url": "/react_with_tania_rascia/index.html"
+  },
+  {
+    "revision": "770d598bd1124177e65c",
+    "url": "/react_with_tania_rascia/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "bced947ef4ae0ed66063",
+    "url": "/react_with_tania_rascia/static/js/2.ebb8b90c.chunk.js"
+  },
+  {
+    "revision": "770d598bd1124177e65c",
+    "url": "/react_with_tania_rascia/static/js/main.6d03df62.chunk.js"
+  },
+  {
+    "revision": "23f0361177c82b3559d7",
+    "url": "/react_with_tania_rascia/static/js/runtime-main.58f760af.js"
+  }
+]);
